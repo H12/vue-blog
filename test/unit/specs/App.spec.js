8499c8a3602ecd536b8ae06fec736890b00b5588
Henry Firth <henrynf12@gmail.com>
2017-11-12 17:04:46 -0600

    Add tests for APp

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -13,7 +13,38 @@ describe('App.vue', () => {
   });
 
   it('should render correct contents', () => {
+    expect(app.$children.length).to.equal(2);
     expect(app.$options.components).to.have.property('Bio');
     expect(app.$options.components).to.have.property('Blog');
+  });
+
+  describe('methods', () => {
+    describe('expandBio', () => {
+      it('should expand the blog when isBioCollapsed is true', () => {
+        app.$data.isBioCollapsed = true;
+        app.expandBio();
+        expect(app.$data.isBioCollapsed).to.be.false;
+      });
+
+      it('should do nothing when isBioCollapsed is false', () => {
+        app.$data.isBioCollapsed = false;
+        app.expandBio();
+        expect(app.$data.isBioCollapsed).to.be.false;
+      });
+    });
+
+    describe('expandBlog', () => {
+      it('should expand the blog when isBlogCollapsed is true', () => {
+        app.$data.isBlogCollapsed = true;
+        app.expandBlog();
+        expect(app.$data.isBlogCollapsed).to.be.false;
+      });
+
+      it('should do nothing when isBlogCollapsed is false', () => {
+        app.$data.isBlogCollapsed = false;
+        app.expandBlog();
+        expect(app.$data.isBlogCollapsed).to.be.false;
+      });
+    });
   });
 });
